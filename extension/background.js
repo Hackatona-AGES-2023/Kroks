e1e1674f46683b35ed803339aa17ad6bf7b2f02b
PZard <pedrozardinguimaraes@gmail.com>
2023-05-27 07:35:56 -0300

    Add UI to component

--- extension/background.js
+++ extension/background.js
@@ -1,26 +1,21 @@
-function reddenPage() {
-  document.body.style.backgroundColor = "red";
-  const metas = document.head.getElementsByTagName('meta');
+function readPage() {
+  const body = document.body.innerHTML
+  const bodyArray = []
 
-  const body = document.body.innerHTML;
-  const bodyArray = [];
-
-  // separate body into array of 1900 caracters
   for (let i = 0; i < body.length; i += 1900) {
-    bodyArray.push(body.substring(i, i + 1900));
+    bodyArray.push(body.substring(i, i + 1900))
   }
 
   fetch("http://localhost:3000/test", {
     method: "POST",
-    body: JSON.stringify(bodyArray),
+    body: JSON.stringify(bodyArray)
   })
 }
 
-chrome.action.onClicked.addListener((tab) => {
-  if (!tab.url.includes("chrome://")) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: reddenPage,
-    });
-  }
-});
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+  chrome.tabs.executeScript({
+    target: details.tabId,
+    function: readPage
+  })
+})
+
