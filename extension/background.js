6810ac998b291f57f0ca3f01e0529aebaa6696bf
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-05-27 07:22:35 -0300

    uhu

--- extension/background.js
+++ extension/background.js
@@ -1,19 +1,22 @@
 function reddenPage() {
-  document.body.style.backgroundColor = "red";
-  const metas = document.head.getElementsByTagName('meta');
-
   const body = document.body.innerHTML;
-  const bodyArray = [];
+  const imgs = document.getElementsByTagName("img");
+  const imageUrls = [];
 
-  // separate body into array of 1900 caracters
-  for (let i = 0; i < body.length; i += 1900) {
-    bodyArray.push(body.substring(i, i + 1900));
+  for (let i = 0; i < imgs.length; i++) {
+    imageUrls.push(imgs[i].src);
   }
 
   fetch("http://localhost:3000/test", {
     method: "POST",
-    body: JSON.stringify(bodyArray),
-  })
+    body: JSON.stringify([body]),    
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(v => v.json()).then(v => {
+      console.log(v)
+      document.body.innerHTML = v.join("")
+    })
 }
 
 chrome.action.onClicked.addListener((tab) => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+import { json } from "stream/consumers";
 require("dotenv").config();
-
 const server = fastify();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
+});
+
+server.register(cors, {
+  // put your options here
 });
 
 const openai = new OpenAIApi(configuration);
@@ -15,14 +20,18 @@ server.post("/test", async (req, res) => {
 
   messages.push({
     role: "system",
-    content: "Você é um assistente virtual de acessibilidade.",
+    content:
+      "Você é um assistente virtual de acessibilidade. Você pode adicionar roles e aria para tornar os htmls enviados mais acessível só retorne código",
   });
+
+  let count = 0;
 
   for (const reqElement of req.body as string[]) {
     messages.push({
       role: "user",
-      content: `Você pode adicionar roles e aria para tornar esse html mais acessível: ${req}`,
+      content: `${reqElement}`,
     });
+    console.log(count++);
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: messages,
